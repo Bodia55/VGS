10970354b38df87821f3c11a4d3fd2b7be76dc2d
Bodia <bodyfarahat@gmail.com>
2023-03-14 07:04:29 -0700

    nothing

--- PROFILES/models.py
+++ PROFILES/models.py
@@ -2,9 +2,8 @@ from django.db import models
 import uuid
 
 class Profile(models.Model):
-    guc_id = models.CharField(max_length=200)
+    id = models.CharField(max_length=200)
     password = models.CharField(max_length=2000)
-    xp = models.PositiveBigIntegerField(default=0, null=True, blank=True)
     sessionId = models.UUIDField(default=uuid.uuid4, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
