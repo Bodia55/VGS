693da34c3be7b4ba489b3419f813c05062e20687
Bodia55 <85449203+Bodia55@users.noreply.github.com>
2023-03-21 21:47:52 +0200

    Update settings.py

--- VGS/settings.py
+++ VGS/settings.py
@@ -35,7 +35,7 @@ DEBUG = True
 # for production add 127.0.0.1 as an allowed host
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['https://vgs-production.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['https://vgs-production.up.railway.app']
 CSRF_COOKIE_DOMAIN = 'railway.app'
 
 
